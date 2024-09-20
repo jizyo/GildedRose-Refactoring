8167778e3487e95b4e242ebc597e9ffff2f44986
Adrian Koch <adrianmkoch@hotmail.com>
2024-09-20 15:50:14 +0200

    refactored

--- gilded_rose.js
+++ gilded_rose.js
@@ -1,8 +1,75 @@
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
-    this.sellIn = sellIn;
+    this.sellIn = sellIn; // variable name not clear enough
     this.quality = quality;
+  }
+}
+
+class AgedBrie extends Item{
+  constructor(sellIn, quality){
+    super('Aged Brie', sellIn, quality)
+  }
+  
+  updateQuality() {
+    this.sellIn--; 
+
+    if (this.quality < 50) {
+      this.quality++; 
+    }
+
+    if (this.sellIn < 0 && this.quality < 50) {
+      this.quality++;
+    }
+  }
+}
+
+class BackstagePass extends Item{
+  constructor(sellIn, quality){
+    super('Backstage passes to a TAFKAL80ETC concert', sellIn, quality)
+  }
+  updateQuality() {
+    this.sellIn--;
+
+    if (this.quality < 50) {
+      this.quality++; 
+    }
+
+    if (this.sellIn < 0 && this.quality < 50) {
+      this.quality++;
+    }
+
+    if (this.sellIn <= 10){
+      if(this.sellIn <= 5){
+        this.quality + 2;
+        return;
+      }
+      this.quality++;
+    }
+  }
+}
+
+class Sulfuras extends Item{
+  constructor(sellIn, quality){
+    super('Sulfuras, Hand of Ragnaros', sellIn, quality)
+  }
+}
+
+class ConjuredItem extends Item{
+  constructor(sellIn, quality){
+    super('Conjured Item', sellIn, quality)
+  }
+  
+  updateQuality() {
+    this.sellIn--; 
+
+    if (this.quality < 50) {
+      this.quality - 2; 
+    }
+
+    if (this.sellIn < 0 && this.quality < 50) {
+      this.quality - 2;
+    }
   }
 }
 
@@ -10,6 +77,51 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+
+  updateQuality() {
+    this.items.forEach(item => {
+      if (item instanceof AgedBrie) {
+        item.updateQuality();
+        return
+      } 
+      
+      if (item instanceof BackstagePass) {
+        item.updateQuality(); 
+        return
+      } 
+
+      if (item instanceof ConjuredItem) {
+        item.updateQuality(); 
+        return
+      } 
+
+      if (!item instanceof Sulfuras){
+        // Standard-Item-Logik
+        item.sellIn--;
+        if (item.quality > 0) {
+          item.quality--;
+        }
+        if (item.sellIn < 0 && item.quality > 0) {
+          item.quality--;
+        }
+      }
+    
+    });
+    return this.items;
+  }
+}
+
+module.exports = {
+  Item,
+  AgedBrie,
+  BackstagePass,
+  Sulfuras,
+  ConjuredItem,
+  Shop
+}
+
+
+/*
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -18,7 +130,7 @@ class Shop {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
-      } else {
+      } else {//Qualität dieser artikel sinkt nicht, qualität nimmt zu
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -58,10 +170,4 @@ class Shop {
     }
 
     return this.items;
-  }
-}
-
-module.exports = {
-  Item,
-  Shop
-}
+  }*/
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-const { Item, Shop } = require('./gilded_rose');
+const { Item, AgedBrie, BackstagePass, Sulfuras, Shop, ConjuredItem } = require('./gilded_rose');
 
 // Create items
 const items = [
-  new Item('Aged Brie', 10, 20),
-  new Item('Sulfuras, Hand of Ragnaros', 5, 80),
-  new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-  new Item('Regular Item', 10, 7)
+  new AgedBrie(10, 20),
+  new Sulfuras(5, 80),
+  new BackstagePass(15, 20),
+  new Item('Regular Item', 10, 7),
+  new ConjuredItem( 10, 7)
 ];
 
 // Create a shop with the items

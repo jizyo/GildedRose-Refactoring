bf20d43101becf1cf2d5eeed51335f2ef6f1faa8
Adrian Koch <adrianmkoch@hotmail.com>
2024-09-27 15:36:19 +0200

    added old gilded rose code

--- gilded_rose.js
+++ gilded_rose.js
@@ -1,75 +1,8 @@
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
-    this.sellIn = sellIn; 
+    this.sellIn = sellIn;
     this.quality = quality;
-  }
-}
-
-class AgedBrie extends Item{
-  constructor(sellIn, quality){
-    super('Aged Brie', sellIn, quality)
-  }
-  
-  updateQuality() {
-    this.sellIn--; 
-
-    if (this.quality < 50) {
-      this.quality++; 
-    }
-
-    if (this.sellIn < 0 && this.quality < 50) {
-      this.quality++;
-    }
-  }
-}
-
-class BackstagePass extends Item{
-  constructor(sellIn, quality){
-    super('Backstage passes to a TAFKAL80ETC concert', sellIn, quality)
-  }
-  updateQuality() {
-    this.sellIn--;
-
-    if (this.quality < 50) {
-      this.quality++; 
-    }
-
-    if (this.sellIn < 0 && this.quality < 50) {
-      this.quality++;
-    }
-
-    if (this.sellIn <= 10){
-      if(this.sellIn <= 5){
-        this.quality + 2;
-        return;
-      }
-      this.quality++;
-    }
-  }
-}
-
-class Sulfuras extends Item{
-  constructor(sellIn, quality){
-    super('Sulfuras, Hand of Ragnaros', sellIn, quality)
-  }
-}
-
-class ConjuredItem extends Item{
-  constructor(sellIn, quality){
-    super('Conjured Item', sellIn, quality)
-  }
-  
-  updateQuality() {
-    this.sellIn--; 
-
-    if (this.quality < 50) {
-      this.quality - 2; 
-    }
-
-    if (this.sellIn < 0 && this.quality < 50) {
-      this.quality - 2;
-    }
   }
 }
 
@@ -77,40 +10,6 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
-
-  updateQuality() {
-    this.items.forEach(item => {
-      if (item instanceof AgedBrie || item instanceof BackstagePass || item instanceof ConjuredItem) {
-        item.updateQuality();
-        return;
-      }
-
-      if (!(item instanceof Sulfuras)){
-        item.sellIn--;
-        if (item.quality > 0) {
-          item.quality--;
-        }
-        if (item.sellIn < 0 && item.quality > 0) {
-          item.quality--;
-        }
-      }
-    
-    });
-    return this.items;
-  }
-}
-
-module.exports = {
-  Item,
-  AgedBrie,
-  BackstagePass,
-  Sulfuras,
-  ConjuredItem,
-  Shop
-}
-
-
-/*
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -119,7 +18,7 @@ module.exports = {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
-      } else {//Qualität dieser artikel sinkt nicht, qualität nimmt zu
+      } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -159,4 +58,9 @@ module.exports = {
     }
 
     return this.items;
-  }*/
+  }
+}
+module.exports = {
+  Item,
+  Shop
+}
